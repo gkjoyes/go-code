8ca892b7c446560bb413a2106196e21d50139aaf
george-kj <georgekuttyjoyes@gmail.com>
2019-05-02 21:24:07 +0530

    Completed Goroutine lesson

---
+++ concurrency/goroutines/Readme.md
@@ -0,0 +1,4 @@
+# Goroutines
+
+Goroutines are functions that are created and scheduled to be run independently by the Go scheduler. The Go scheduler is responsible for the
+management and execution of goroutines.
---
+++ concurrency/goroutines/example2/example2.go
@@ -0,0 +1,64 @@
+// Sample program to show how the goroutine scheduler will
+// time slice goroutines on a single thread.
+
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
+)
+
+func init() {
+
+	// Allocate one logical processor for the scheduler to use.
+	runtime.GOMAXPROCS(1)
+}
+
+func main() {
+
+	// wg is used to manage concurrency.
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Create Goroutines")
+
+	// Create first goroutine and mange its lifecycle here.
+	go func() {
+		printHashes("A")
+		wg.Done()
+	}()
+
+	// Create second goroutine and manage its lifecycle here.
+	go func() {
+		printHashes("B")
+		wg.Done()
+	}()
+
+	// Wait for the goroutines to finish.
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+
+	fmt.Println("Terminating Program")
+}
+
+// printHashes calculates the sha1 hash for a range of
+// numbers and prints each in hex encoding.
+func printHashes(prefix string) {
+
+	for i := 0; i <= 50000; i++ {
+
+		// Convert i to a string.
+		num := strconv.Itoa(i)
+
+		// Calculate hash for string num.
+		sum := sha1.Sum([]byte(num))
+
+		// Print prefix: 5-digit-number: hex encoded hash
+		fmt.Printf("%s: %5d: %x\n", prefix, i, sum)
+	}
+
+	fmt.Println("Completed", prefix)
+}
